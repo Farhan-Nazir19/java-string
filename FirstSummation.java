5859da224852324c21e5f8580bd28ea4376547cf
Farhan-Nazir19 <farhannazir200319@gmail.com>
2025-02-17 21:07:15 +0600

    Add files via upload

---
+++ FirstSummation.java
@@ -0,0 +1,9 @@
+public class FirstSummation
+{public static void main (String [] args)
+  {int number1 = 10;
+  int number2 =13;
+  int sum= number1 + number2;
+  System.out.println("Summation is :"+ sum);
+  System.out.print("End");
+}
+}
