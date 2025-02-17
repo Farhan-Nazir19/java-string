ef9bae94e9bb9f739898d0c48f6099db57e5f3dd
Farhan-Nazir19 <farhannazir200319@gmail.com>
2025-02-17 21:06:21 +0600

    Add files via upload

---
+++ Declaration_and_Assignment.java
@@ -0,0 +1,11 @@
+public class Declaration_and_Assignment
+{
+  public static void main(String [] args)
+  { int num1; //declaration
+    num1=12; //assignment
+    int num2;
+    num2=11; //assignment
+      System.out.println(num1);
+      System.out.println(num2);
+  }
+}
