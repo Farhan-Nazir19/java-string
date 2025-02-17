1b81ad0c20b0d02bd58fe791f2c7e45bf0b337bb
Farhan-Nazir19 <farhannazir200319@gmail.com>
2025-02-17 20:04:27 +0600

    Update and rename PrintingExample.java to StringName.exp

--- PrintingExample.java
+++
@@ -1,8 +0,0 @@
-public class PrintingExample{   
-  public static void main(String[] args)  { 
-    double y = 1.56;
-    System.out.print("Number: \n" +  23);
-    System.out.println("Hello World"); 
-    System.out.printf("Formatted y = %.7f\n", y);
-  }   
-}
